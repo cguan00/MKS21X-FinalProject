cef62bd359316d1c0f899ec3a9f8eea9d7216d08
Christy Guan <cguan00@stuy.edu>
2019-01-04 14:47:04 -0500

    added comments to describe fields

--- Piece.java
+++ Piece.java
@@ -1,9 +1,12 @@
 public abstract class Piece{
-  //private Player color;
-  //did not create Player class yet
+  private Player color;
+  //player color: either white or black
 
   private Square location;
+  //location of the Piece on the board
+
   private boolean moved;
+  //keeps tracks of if the piece was moved or not
 
   public abstract boolean checkValidMove();
 
