25d5e43b25757baf93e264581a544c685463a60e
Sharon Zou <szou00@stuy.edu>
2019-01-04 05:27:30 -0500

    toString created to print an empty 8 by 8 board

--- Board.java
+++ Board.java
@@ -6,4 +6,21 @@ public class Board{
   public Board(){
     Square[][] squares = new Square[8][8];
   }
+
+  public String toString() {
+    String ans = "";
+    for (int r = 0; r < 9; r++) {
+      ans += "\n" + "|";
+      for (int c = 0; c < 9; c++) {
+        ans += " _";
+      }
+      ans += " |";
+    }
+    return ans;
+  }
+
+  public static void main(String[] args) {
+    Board newBoard = new Board();
+    System.out.println(newBoard);
+  }
 }
