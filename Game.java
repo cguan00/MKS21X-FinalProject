4e17ddb0e3bee6f66f3b8a714ad784c1510bc0a0
szou00 <szou00@stuy.edu>
2019-01-04 14:41:19 -0500

    comments added to toString and some things fixed

--- Game.java
+++ Game.java
@@ -3,24 +3,27 @@ public class Game {
   private Player turn;
   //private List<Move> moves;
 
+  //prints the board
   public String toString() {
     String ans = "";
     String letter = "ABCDEFGH";
     for (int r = 0; r < 9; r++) {
-      if (r != 0) {
-        ans += "\n" + r;
+      if (r == 0) { //if it's the first row numbers will be printed on the side
+        ans += " "; //excluding the first row
       }
       else {
-        ans += " ";
+        ans += "\n" + r;
       }
-      for (int c = 0; c < 9; c++) {
+      for (int c = 0; c < 9; c++) { //letters are printed above each column
         if (r == 0) {
-          if (c != 0) {
+          if (c != 0) { //exlcluding the first column
             ans += " " + letter.charAt(c - 1);
           }
         }
-        if (c != 8 && r != 0) {
-          ans += " _";
+        if (r != 0) {
+          if (c != 0) {
+            ans += " _";
+          }
         }
       }
     }
