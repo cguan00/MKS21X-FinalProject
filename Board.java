fe635c61633c99614d311c7013064628d64bb102
Christy Guan <cguan00@stuy.edu>
2019-01-03 23:54:35 -0500

    created Board class with two fields and constructor

---
+++ Board.java
@@ -0,0 +1,9 @@
+public class Board{
+  private Square[][] squares;
+  //private Piece[][] pieceSets;
+  //did not create Piece class yet
+
+  public Board(){
+    Square[][] squares = new Square[8][8];
+  }
+}
