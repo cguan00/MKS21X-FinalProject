05a89619df6a80da48232616bb7eb6b0fdf4f048
Sharon Zou <szou00@stuy.edu>
2019-01-04 06:11:58 -0500

    Moved the toString of Board.java to Game.java

--- Game.java
+++ Game.java
@@ -3,4 +3,32 @@ public class Game {
   private Player turn;
   //private List<Move> moves;
 
+  public String toString() {
+    String ans = "";
+    String letter = "ABCDEFGH";
+    for (int r = 0; r < 9; r++) {
+      if (r != 0) {
+        ans += "\n" + r;
+      }
+      else {
+        ans += " ";
+      }
+      for (int c = 0; c < 9; c++) {
+        if (r == 0) {
+          if (c != 0) {
+            ans += " " + letter.charAt(c - 1);
+          }
+        }
+        if (c != 8 && r != 0) {
+          ans += " _";
+        }
+      }
+    }
+    return ans;
+  }
+
+  public static void main(String[] args) {
+    Game newGame = new Game();
+    System.out.println(newGame);
+  }
 }
