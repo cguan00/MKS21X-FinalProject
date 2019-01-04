8e9e6a40a46545b2257b6d68c08711a835923123
Christy Guan <cguan00@stuy.edu>
2019-01-04 00:19:01 -0500

    created class, fields, and abstract method

---
+++ Piece.java
@@ -0,0 +1,10 @@
+public abstract class Piece{
+  //private Player color;
+  //did not create Player class yet
+
+  private Square location;
+  private boolean moved;
+
+  public abstract boolean checkValidMove();
+
+}
