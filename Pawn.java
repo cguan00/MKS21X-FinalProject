69b1f0d73b82579580e681573b86d17fe7e59f37
Christy Guan <cguan00@stuy.edu>
2019-01-05 14:53:31 -0500

    updated checkValidMove. must write Square class before finishing method

--- Pawn.java
+++ Pawn.java
@@ -1,9 +1,38 @@
 public class Pawn extends Piece{
-  public boolean promoted;
-  public Piece promoteTo;
+  private Player color;
+  //player color: either white or black
 
-  public boolean checkValidMove(){
-    return true;
+  private Square location;
+  //location of the Piece on the board
+
+  private boolean moved;
+  //keeps tracks of if the piece was moved or not
+
+  public boolean promoted;
+  //set to true if the Pawn has been promoted
+
+  public Piece promoteTo;
+  //if promoted, keep track of which Piece the Pawn has been changed to
+
+  public Pawn(Player playerColor, Square loc){
+    super(playerColor, loc);
+  }
+
+  public boolean checkValidMove(Square newLocation){
+    int currentRow = location.getRow();
+    int currentCol = location.getCol();
+    int newRow = newLocation.getRow();
+    int newCol = newLocation.getCol();
+     if(color.equals("white")){
+       if(newRow > currentRow){
+         return false;
+       }
+     }
+     if(color.equals("black")){
+       if(newRow < currentRow){
+         return false;
+       }
+     }
   }
 
 
