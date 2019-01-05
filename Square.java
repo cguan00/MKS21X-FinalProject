4f91c24a9b9a1d25cc02772d7241e3a3ada5e786
Christy Guan <cguan00@stuy.edu>
2019-01-05 14:58:28 -0500

    added get methods for row and col

--- Square.java
+++ Square.java
@@ -1,4 +1,18 @@
 public class Square{
   public int row;
   public int col;
+
+  public Square(int inputRow, int inputCol){
+    row = inputRow;//set row to inputted value
+    col = inputCol;//set col to inputted value
+  }
+
+  public int getRow(){
+    return row;//returns int row of the Square
+  }
+
+  public int getCol(){
+    return col;//returns int col of the Square
+  }
+
 }
