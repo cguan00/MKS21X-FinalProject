c2c9b35d56b386558ebdf93f3f9278bbc209a43c
Christy Guan <cguan00@stuy.edu>
2019-01-05 15:14:27 -0500

    updated fields and created constructor

--- Rook.java
+++ Rook.java
@@ -1,5 +1,18 @@
 public class Rook extends Piece{
-  public boolean checkValidMove(){
+  private Player color;
+  //player color: either white or black
+
+  private Square location;
+  //location of the Piece on the board
+
+  private boolean moved;
+  //keeps tracks of if the piece was moved or not
+
+  public Rook(Player playerColor, Square loc){
+    super(playerColor, loc);
+  }
+
+  public boolean checkValidMove(Square newLocation){
     return true;
   }
 
