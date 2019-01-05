d710a020aa2e4fcb4253eddfc889ba53129fff7d
Christy Guan <cguan00@stuy.edu>
2019-01-05 15:20:22 -0500

    updated fields and checkValidMove. added constructor and toString

--- Bishop.java
+++ Bishop.java
@@ -1,6 +1,27 @@
 public class Bishop extends Piece{
-  public boolean checkValidMove(){
+  private Player color;
+  //player color: either white or black
+
+  private Square location;
+  //location of the Piece on the board
+
+  private boolean moved;
+  //keeps tracks of if the piece was moved or not
+
+  public Bishop(Player playerColor, Square loc){
+    super(playerColor, loc);
+  }
+
+  public boolean checkValidMove(Square newLocation){
     return true;
+  }
+
+  public String toString(){
+    if(color.equals("white")){
+      return "B";//white pieces are capitalized
+    }else{
+      return "b";//black pieces are lowercase
+    }
   }
 
 }
