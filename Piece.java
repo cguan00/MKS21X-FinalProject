1ba6f7b6ccf2aeb7a8240ecb3685f2701892cc5f
Sharon Zou <szou00@stuy.edu>
2019-01-06 06:46:28 -0500

    copied methods that Christy wrote to avoid merging problems

--- Piece.java
+++ Piece.java
@@ -1,10 +1,27 @@
 public abstract class Piece{
-  //private Player color;
-  //did not create Player class yet
+  private Player color;
+  //player color: either white or black
 
   private Square location;
-  private boolean moved;
+  //location of the Piece on the board
 
-  public abstract boolean checkValidMove();
+  private boolean moved;
+  //keeps tracks of if the piece was moved or not
+
+  public Piece(Player playerColor, Square loc){
+    color = playerColor;//keeps track of Player color, either black or white
+    location = loc;//keeps track of which Square the Piece is located on
+    moved = false;//just created the piece, so hasn't moved yet
+  }
+
+  public Player getColor(){
+    return color;//returns the Player color, either white or black
+  }
+
+  public Square getLocation(){
+    return location;//returns the Square the Piece is located at
+  }
+
+  public abstract boolean checkValidMove(Square newLocation);
 
 }
