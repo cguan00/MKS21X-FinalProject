c021b26b29bd2822128d90d6f7221a0509e80c20
Sharon Zou <szou00@stuy.edu>
2019-01-05 22:10:31 -0500

    pieceSets[][] created

--- Board.java
+++ Board.java
@@ -1,10 +1,10 @@
 public class Board{
   private Square[][] squares;
-  //private Piece[][] pieceSets;
-  //did not create Piece class yet
+  private Piece[][] pieceSets;
 
   public Board(){
     Square[][] squares = new Square[8][8];
+    Piece[][] pieces = new Piece[8][8]; //what should the dimensions be?
   }
 
 }
