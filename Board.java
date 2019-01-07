aa944e17605bcb9f77ac78c5f2a7305def31dcc7
szou00 <szou00@stuy.edu>
2019-01-07 14:51:54 -0500

    create method in process

--- Board.java
+++ Board.java
@@ -2,14 +2,21 @@ public class Board{
   private Square[][] squares;
   private Piece[][] pieceSets;
 
-  public Board(){
+  public void create(){
     Square[][] squares = new Square[8][8];
-    Piece[][] pieces = new Piece[4][16]; //what should the dimensions be?
+    Piece[][] pieces = new Piece[2][16];
+    for (int i = 0; i < 2; i++) {
+      for (int x = 0; x < 16; i++) {
+
+      }
+    }
   }
 
+/**
   //returns the pieceSets
   public Piece[][] getPieceSets() {
     return pieceSets;
   }
+**/
 
 }
