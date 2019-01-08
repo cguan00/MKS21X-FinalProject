734d26d37e1a199db6590476c4717a6e282ed300
Christy Guan <cguan00@stuy.edu>
2019-01-07 21:33:04 -0500

    added conditionals to check valid moves

--- King.java
+++ King.java
@@ -12,8 +12,32 @@ public class King extends Piece{
     super(playerColor, loc);
   }
 
-  public boolean checkValidMove(Square newLocation){
-    return true;
+  public boolean checkValidMove(Square newLocation) throws IllegalArgumentException{
+    //assign variables to hold int values of rows and cols to avoid repetitive code
+    int currentRow = location.getRow();
+    int currentCol = location.getCol();
+    int newRow = newLocation.getRow();
+    int newCol = newLocation.getCol();
+
+    //if the Square is not part of the 8x8 board, throw exception
+    if(newRow < 0 || newRow >= 9 || newCol < 0 || newCol >= 9){
+      throw new IllegalArgumentException();
+    }
+
+    //use variables to store how far the piece is trying to move in either direction
+    int rowDiff = Math.abs(newRow - currentRow);
+    int colDiff = Math.abs(newCol - currentCol);
+
+    if(rowDiff == 1 && colDiff == 0 ){//move one row up or down
+      return true;
+    }
+    if(colDiff == 1 && rowDiff == 0){//move one col to left or right
+      return true;
+    }
+    if(colDiff == 1 && rowDiff == 1){//move diagonally by one square
+      return true;
+    }
+    return false;
   }
 
   public String toString(){
