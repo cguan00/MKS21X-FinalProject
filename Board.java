090ac13463d7065dd10d9d20502c3ffd598755f9
Sharon Zou <szou00@stuy.edu>
2019-01-08 05:37:26 -0500

    toString updated

--- Board.java
+++ Board.java
@@ -3,8 +3,8 @@ public class Board{
   private Piece[][] pieceSets;
 
   public void create(){
-    Square[][] squares = new Square[8][8];
-    Piece[][] pieces = new Piece[2][16];
+    squares = new Square[8][8];
+    pieceSets = new Piece[2][16];
     Player color = new Player("black");
     for (int i = 0; i < 2; i++) {
       for (int x = 0; x < 16; x++) {
@@ -12,29 +12,60 @@ public class Board{
           color = new Player("white");
         }
         if (x == 0 && x == 7) {
-          pieces[i][x] = new Rook(color, squares[i][x]);
+          pieceSets[i][x] = new Rook(color, squares[i][x]);
         }
         if (x == 1 && x == 6) {
-          pieces[i][x] = new Knight(color, squares[i][x]);
+          pieceSets[i][x] = new Knight(color, squares[i][x]);
         }
         if (x == 2 && x == 5) {
-          pieces[i][x] = new Bishop(color, squares[i][x]);
+          pieceSets[i][x] = new Bishop(color, squares[i][x]);
         }
         if (x == 3) {
-          pieces[i][x] = new Queen(color, squares[i][x]);
+          pieceSets[i][x] = new Queen(color, squares[i][x]);
         }
         if (x == 4) {
-          pieces[i][x] = new King(color, squares[i][x]);
+          pieceSets[i][x] = new King(color, squares[i][x]);
         }
         if (x > 7) {
-          pieces[i][x] = new Pawn(color, squares[i+1][x-8]);
+          pieceSets[i][x] = new Pawn(color, squares[i+1][x-8]);
         }
       }
     }
   }
 
-  public static void main(String[] args) {
+  public String toString() {
+    String ans = "";
+    String letter = "ABCDEFGH";
+    for (int r = 0; r < 9; r++) {
+      if (r == 0) { //if it's the first row numbers will be printed on the side
+        ans += " "; //excluding the first row
+      }
+      else {
+        ans += "\n" + r;
+      }
+      for (int c = 0; c < 9; c++) { //letters are printed above each column
+        if (r == 0) {
+          if (c != 0) { //exlcluding the first column
+            ans += " " + letter.charAt(c - 1);
+          }
+        }
+        if (r == 1 && c > 0) {
+          ans += (pieceSets[1][c-1]);
+        }
+        if (r == 2 && c > 0) {
+          ans += (pieceSets[1][c+7]);
+        }
+        if (r != 1 && r!= 2 && c != 0) {
+          ans += " _";
+        }
+      }
+    }
+    return ans;
+  }
 
+  public static void main(String[] args) {
+    Board newBoard = new Board();
+    System.out.println(newBoard);
   }
 
 /**
