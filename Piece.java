61c820b45675e35e9e3bfe054100c4a1a86bdd4a
Christy Guan <cguan00@stuy.edu>
2019-01-08 23:49:20 -0500

    added board field

--- Piece.java
+++ Piece.java
@@ -1,4 +1,7 @@
 public abstract class Piece{
+  private Board board;
+  //stores the game board. must have access to Squares
+
   private Player color;
   //player color: either white or black
 
@@ -8,7 +11,8 @@ public abstract class Piece{
   private boolean moved;
   //keeps tracks of if the piece was moved or not
 
-  public Piece(Player playerColor, Square loc){
+  public Piece(Board gameBoard, Player playerColor, Square loc){
+    board = gameBoard;//the board the game is played on
     color = playerColor;//keeps track of Player color, either black or white
     location = loc;//keeps track of which Square the Piece is located on
     moved = false;//just created the piece, so hasn't moved yet
@@ -23,5 +27,6 @@ public abstract class Piece{
   }
 
   public abstract boolean checkValidMove(Square newLocation);
+
 
 }
