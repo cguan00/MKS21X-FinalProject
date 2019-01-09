efe2666a53750a80a0c4e3047530b6d1c482fe02
Sharon Zou <szou00@stuy.edu>
2019-01-09 14:11:53 -0500

    toString changed so that it'll print out two rows, one row of black pieces and another row of white pieces

--- Game.java
+++ Game.java
@@ -45,27 +45,37 @@ public class Game {
 
   //prints the board
   public String toString() {
+    // String ans = "";
+    // String letter = "ABCDEFGH";
+    // for (int r = 0; r < 9; r++) {
+    //   if (r == 0) { //if it's the first row numbers will be printed on the side
+    //     ans += " "; //excluding the first row
+    //   }
+    //   else {
+    //     ans += "\n" + r;
+    //   }
+    //   for (int c = 0; c < 9; c++) { //letters are printed above each column
+    //     if (r == 0) {
+    //       if (c != 0) { //exlcluding the first column
+    //         ans += " " + letter.charAt(c - 1);
+    //       }
+    //     }
+    //     if (r != 0) {
+    //       if (c != 0) {
+    //         ans += " _";
+    //       }
+    //     }
+    //   }
+    // }
+    // return ans;
+
     String ans = "";
-    String letter = "ABCDEFGH";
-    for (int r = 0; r < 9; r++) {
-      if (r == 0) { //if it's the first row numbers will be printed on the side
-        ans += " "; //excluding the first row
-      }
-      else {
-        ans += "\n" + r;
-      }
-      for (int c = 0; c < 9; c++) { //letters are printed above each column
-        if (r == 0) {
-          if (c != 0) { //exlcluding the first column
-            ans += " " + letter.charAt(c - 1);
-          }
-        }
-        if (r != 0) {
-          if (c != 0) {
-            ans += " _";
-          }
-        }
-      }
+    for (int i = 0; i < 16; i++) {
+      ans += board.getPiece(0,i) + " ";
+    }
+    ans += "\n";
+    for (int i = 0; i < 16; i++) {
+      ans += board.getPiece(1,i) + " ";
     }
     return ans;
   }
