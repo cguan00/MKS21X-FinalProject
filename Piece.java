eba6ea416f7b624c7608c924d9ae19c80ae6c33b
Christy Guan <cguan00@stuy.edu>
2019-01-09 14:39:28 -0500

    created a default constructor

--- Piece.java
+++ Piece.java
@@ -2,7 +2,7 @@ public abstract class Piece{
   private Board board;
   //stores the game board. must have access to Squares
 
-  private Player color;
+  private String color;
   //player color: either white or black
 
   private Square location;
@@ -11,14 +11,18 @@ public abstract class Piece{
   private boolean moved;
   //keeps tracks of if the piece was moved or not
 
-  public Piece(Board gameBoard, Player playerColor, Square loc){
+  public Piece(){
+    
+  }
+
+  public Piece(Board gameBoard, String playerColor, Square loc){
     board = gameBoard;//the board the game is played on
     color = playerColor;//keeps track of Player color, either black or white
     location = loc;//keeps track of which Square the Piece is located on
     moved = false;//just created the piece, so hasn't moved yet
   }
 
-  public Player getColor(){
+  public String getColor(){
     return color;//returns the Player color, either white or black
   }
 
