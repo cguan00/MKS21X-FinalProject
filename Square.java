baa49ab1bb71b6cbbfa2135b878cbb4608e0d4fc
szou00 <szou00@stuy.edu>
2019-01-10 14:23:51 -0500

    contents updated for merge

--- Square.java
+++ Square.java
@@ -1,6 +1,7 @@
 public class Square{
   public int row;
   public int col;
+  private Piece piece;
 
   public Square(int inputRow, int inputCol){
     row = inputRow;//set row to inputted value
@@ -13,6 +14,14 @@ public class Square{
 
   public int getCol(){
     return col;//returns int col of the Square
+  }
+
+  public void storePiece(Piece newPiece) {
+    piece = newPiece;
+  }
+
+  public Piece getPiece() {
+    return piece;
   }
 
 }
