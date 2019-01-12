e144b05ece46a2b02b238a28461b2527007ca27a
Christy Guan <cguan00@stuy.edu>
2019-01-12 10:37:15 -0500

    checkValidMove updated. if piece is opponent's piece, you can capture

--- Rook.java
+++ Rook.java
@@ -33,7 +33,7 @@ public class Rook extends Piece{
     //add valid Squares the piece can move to vertically
     for(int i = 0; i < 7; i++){
       if(i != row){//can't move a Piece to the Square it's already on
-        if(board.getSquare(i, col).getPiece() == null){//check if this Square is empty
+        if(board.getSquare(i, col).getPiece() == null){//if this Square is empty
           validSquares.add(board.getSquare(i, col));//add this Square to list of possible Squares
         }
         if(board.getSquare(i, col).getPiece() != null){//if this Square has a piece...
@@ -47,8 +47,13 @@ public class Rook extends Piece{
     //add valid Squares the piece can move to horizontally
     for(int j = 0; j < 7; j++){
       if(j != col){//can't move a Piece to the Square it's already on
-        if(board.getSquare(row, j).getPiece() == null){//check if this Square is empty
+        if(board.getSquare(row, j).getPiece() == null){//if this Square is  empty
           validSquares.add(board.getSquare(row, j));//add this Square to list of possible Squares
+        }
+        if(board.getSquare(row, j).getPiece() != null){//if this Square is NOT empty
+          if(!board.getSquare(row, j).getPiece().getColor().equals(color)){//...and this piece is not part of your own pieceSet (different color)
+            validSquares.add(board.getSquare(row, j));//add this Square to list of possible Squares
+          }
         }
       }
     }
