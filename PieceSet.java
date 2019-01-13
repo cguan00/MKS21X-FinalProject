15956535aed77368bc4ba4ea0bc1d91bf8036cfe
Christy Guan <cguan00@stuy.edu>
2019-01-12 23:58:15 -0500

    changed the parameters of some instantiated objects to match Piece constructor

--- PieceSet.java
+++ PieceSet.java
@@ -11,12 +11,12 @@ public class PieceSet{
   public void initialPieces(Board board){
     if (color.isBlack()) {
       pieces.add(new Rook(board, color, board.getSquare(0,0)));
-      pieces.add(new Knight(color, board.getSquare(0,1)));
+      pieces.add(new Knight(board, color, board.getSquare(0,1)));
       pieces.add(new Bishop(board, color, board.getSquare(0,2)));
       pieces.add(new Queen(board, color, board.getSquare(0,3)));
       pieces.add(new King(board, color, board.getSquare(0,4)));
       pieces.add(new Bishop(board, color, board.getSquare(0,5)));
-      pieces.add(new Knight(color, board.getSquare(0,6)));
+      pieces.add(new Knight(board, color, board.getSquare(0,6)));
       pieces.add(new Rook(board, color, board.getSquare(0,7)));
       for (int i = 0; i < 8; i++) {
         pieces.add(new Pawn(board, color, board.getSquare(1,i)));
@@ -24,12 +24,12 @@ public class PieceSet{
     }
     if (color.isWhite()) {
       pieces.add(new Rook(board, color, board.getSquare(7,0)));
-      pieces.add(new Knight(color, board.getSquare(7,1)));
+      pieces.add(new Knight(board, color, board.getSquare(7,1)));
       pieces.add(new Bishop(board, color, board.getSquare(7,2)));
       pieces.add(new Queen(board, color, board.getSquare(7,3)));
       pieces.add(new King(board, color, board.getSquare(7,4)));
       pieces.add(new Bishop(board, color, board.getSquare(7,5)));
-      pieces.add(new Knight(color, board.getSquare(7,6)));
+      pieces.add(new Knight(board, color, board.getSquare(7,6)));
       pieces.add(new Rook(board, color, board.getSquare(7,7)));
       for (int i = 0; i < 8; i++) {
         pieces.add(new Pawn(board, color, board.getSquare(6,i)));
