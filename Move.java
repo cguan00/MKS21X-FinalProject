e5fa31f5703979c13d37077bef5834e50a758a69
Sharon Zou <szou00@stuy.edu>
2019-01-13 13:32:58 -0500

    added a new constructor

--- Move.java
+++ Move.java
@@ -17,4 +17,10 @@ public class Move {
     board.getSquare(newRow,newColumn).storePiece(currentPiece); //the new square now gets the piece
   }
 
+  public Move(Board board, Player color, Square currentLoc, Square newLoc) {
+    currentPiece = currentLoc.getPiece(); //the piece to be moved is stored
+    currentLoc.storePiece(null); //the original square now doesn't have a piece
+    newLoc.storePiece(currentPiece); //the new square now gets the piece
+  }
+
 }
