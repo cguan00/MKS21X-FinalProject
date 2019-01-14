eb52c5d51ef971d9a9da5a7ad6f62fa9be8e520f
Sharon Zou <szou00@stuy.edu>
2019-01-13 19:22:10 -0500

    more keys added, cursor specificities updated

--- GameDemo.java
+++ GameDemo.java
@@ -27,17 +27,17 @@ public class GameDemo {
     board = new Board();
   }
 
-	public void addMove(String color, String currentLoc, String newLoc) {
-    turn = new Player(color);
-    String columns = "ABCDEFGH";
+	public void addMove(Player color, Square currentLoc, Square newLoc) {
+    turn = color;
+		String columns = "ABCDEFGH";
     String rows = "12345678";
-    int currentRow = rows.indexOf(currentLoc.charAt(1)); //the original row is stored
-    int currentColumn = columns.indexOf(currentLoc.charAt(0)); //the original column is stored
-    int newRow = rows.indexOf(newLoc.charAt(1)); //the new row is stored
-    int newColumn = columns.indexOf(newLoc.charAt(0)); //the new column is stored
-    Piece currentPiece = board.getSquare(currentRow,currentColumn).getPiece(); //the piece to be moved is stored
+    int currentRow = currentLoc.getRow(); //the original row is stored
+    int currentCol = currentLoc.getCol(); //the original column is stored
+    int newRow = newLoc.getRow(); //the new row is stored
+    int newCol = newLoc.getCol(); //the new column is stored
+    Piece currentPiece = board.getSquare(currentRow,currentCol).getPiece(); //the piece to be moved is stored
     //System.out.println(currentPiece.checkValidMove(board.getSquare(currentRow, currentColumn), board.getSquare(newRow,newColumn)));
-    if (currentPiece.checkValidMove(board.getSquare(newRow,newColumn))) {
+    if (currentPiece.checkValidMove(board.getSquare(newRow,newCol))) {
       Move newMove = new Move(board, turn, currentLoc, newLoc);
     }
   }
@@ -59,7 +59,7 @@ public class GameDemo {
     blackP.initialPieces(board); //all pieces are created at starting position
     whiteP.initialPieces(board);
     board.setPieceSets(blackP, whiteP); //sets up the PieceSets in Board so it'll have access
-    board.setSquares(blackP, whiteP); //sets up squares[][] in Board
+    board.setSquares(blackP, whiteP); //sets up square\s[][] in Board
   }
 
 	public Board getBoard() {
@@ -80,25 +80,84 @@ public class GameDemo {
 		size.setColumns(8);
 		terminal.setCursorVisible(false);
 
-		boolean running = true;
 
-		long tStart = System.currentTimeMillis();
-		long lastSecond = 0;
+		// long tStart = System.currentTimeMillis();
+		// long lastSecond = 0;
 		Game newGame = new Game();
 		newGame.create();
 		Player BlackP = new Player("black");
 		Player WhiteP = new Player("white");
 		Player Playing = BlackP; //player black starts first
 		int pressed = 0;
-		Square currentLoc = new Square(0,0);
-		Square newLoc = new Square(0,0);
+		// Square currentLoc = new Square(0,0);
+		// Square newLoc = new Square(0,0);
+
+		//SETTING UP THE BOARD VISUALLY
+		//print out the row of letters on top (A to H)
+		terminal.moveCursor(0,0);//size.getColumns()-size.getColumns() + 5,5);
+		String ans = "";
+		String letter = "ABCDEFGH";
+		for (int r = 0; r < 9; r++) {
+			if (r == 0) { //if it's the first row numbers will be printed on the side
+				terminal.putCharacter(' '); //excluding the first row
+			}
+			else {
+				terminal.moveCursor(0,r);//size.getColumns()-size.getColumns() + 5,5+r);
+				ans += r;
+				terminal.putCharacter(ans.charAt(0));
+				ans = "";
+			}
+			for (int c = 0; c < 9; c++) { //letters are printed above each column
+				if (r == 0) {
+					if (c != 0) { //exlcluding the first column
+						terminal.putCharacter(' ');
+						terminal.putCharacter(letter.charAt(c - 1));
+					}
+				}
+				if (r != 0 && c != 0) {
+					//for every empty space that has a piece in the Squares array
+					//prints out the piece that the Square stores
+					if (newGame.getBoard().getSquare(r-1, c-1).getPiece() == null) {
+						terminal.putCharacter(' ');
+					}
+					else {
+						terminal.putCharacter(' ');
+						ans += newGame.getBoard().getSquare(r-1,c-1).getPiece();
+						terminal.putCharacter(ans.charAt(0));
+						ans = "";
+					}
+					}
+				}
+		}
+
+		boolean running = true;
+
+		//starting coordinates of the cursor
+		x = 5;
+		y = 5;
 
 		while(running){
+			terminal.moveCursor(x,y);
+			terminal.applyBackgroundColor(Terminal.Color.WHITE);
+			terminal.applyForegroundColor(Terminal.Color.BLACK);
+			terminal.applyBackgroundColor(Terminal.Color.DEFAULT);
+			terminal.applyForegroundColor(Terminal.Color.DEFAULT);
+			terminal.applySGR(Terminal.SGR.RESET_ALL);
+			Square currentLoc = new Square(0,0);
+			Square newLoc = new Square(0,0);
 			if (pressed == 1) { //if enter is pressed once, the user is choosing the piece to move
-				currentLoc = newGame.getBoard().getSquare(x,y); //stores the location of the piece
+				int row = y - 1;
+				int col = x / 2 - 1;
+				putString(20,20,terminal,"row" + row);
+				putString(20,21,terminal,"col" + col);
+				currentLoc = newGame.getBoard().getSquare(row,col); //stores the location of the piece
+				putString(20,22,terminal,"current square" + row + ", " + col);
+				putString(20,23,terminal,"piece" + currentLoc.getPiece());
 			}
 			if (pressed == 2) { //if enter is pressed again, the user is choosing the destination of the piece
-				newLoc = newGame.getBoard().getSquare(x,y);
+				int row = y - 1;
+				int col = x / 2 - 1;
+				newLoc = newGame.getBoard().getSquare(row,col);
 				newGame.addMove(Playing, currentLoc, newLoc); //a new move is made based on the previous inputs
 				pressed = 0; //it's now the other player's turn, the value resets
 				if (Playing.isBlack()) { //if black just played, it's white's turn
@@ -108,44 +167,10 @@ public class GameDemo {
 					Playing = BlackP; //if white just played, it's black's turn
 				}
 			}
-			//SETTING UP THE BOARD VISUALLY
-			//print out the row of letters on top (A to H)
-			terminal.moveCursor(0,0);//size.getColumns()-size.getColumns() + 5,5);
-			String ans = "";
-	    String letter = "ABCDEFGH";
-	    for (int r = 0; r < 9; r++) {
-	      if (r == 0) { //if it's the first row numbers will be printed on the side
-	        terminal.putCharacter(' '); //excluding the first row
-	      }
-	      else {
-	        terminal.moveCursor(0,r);//size.getColumns()-size.getColumns() + 5,5+r);
-					ans += r;
-					terminal.putCharacter(ans.charAt(0));
-					ans = "";
-	      }
-	      for (int c = 0; c < 9; c++) { //letters are printed above each column
-	        if (r == 0) {
-	          if (c != 0) { //exlcluding the first column
-							terminal.putCharacter(' ');
-	            terminal.putCharacter(letter.charAt(c - 1));
-	          }
-	        }
-	        if (r != 0 && c != 0) {
-	          //for every empty space that has a piece in the Squares array
-	          //prints out the piece that the Square stores
-	          if (newGame.getBoard().getSquare(r-1, c-1).getPiece() == null) {
-	            terminal.putCharacter(' ');
-	          }
-	          else {
-	            terminal.putCharacter(' ');
-							ans += newGame.getBoard().getSquare(r-1,c-1).getPiece();
-							terminal.putCharacter(ans.charAt(0));
-							ans = "";
-	          }
-	          }
-	        }
-	    }
 
+
+
+			terminal.setCursorVisible(true);
 			Key key = terminal.readInput();
 
 			if (key != null)
@@ -156,22 +181,50 @@ public class GameDemo {
 					terminal.exitPrivateMode();
 					running = false;
 				}
-				if (key.getKind() == Key.Kind.ArrowRight) {
+				if (key.getKind() == Key.Kind.ArrowLeft) {
+					terminal.moveCursor(x,y);
+					//terminal.putCharacter(' ');
+					x--;
+				}
 
-					terminal.exitPrivateMode();
-					running = false;
+				if (key.getKind() == Key.Kind.ArrowRight) {
+					terminal.moveCursor(x,y);
+					//terminal.putCharacter(' ');
+					x++;
+				}
+
+				if (key.getKind() == Key.Kind.ArrowUp) {
+					terminal.moveCursor(x,y);
+					//terminal.putCharacter(' ');
+					y--;
+				}
+
+				if (key.getKind() == Key.Kind.ArrowDown) {
+					terminal.moveCursor(x,y);
+					//terminal.putCharacter(' ');
+					y++;
+				}
+				if (key.getKind() == Key.Kind.valueOf("A")) {
+					terminal.moveCursor(x,y);
+					//terminal.putCharacter(' ');
+					y++;
 				}
 				if (key.getKind() == Key.Kind.Enter) {
 					pressed += 1;
 				}
+				putString(10,10,terminal,"xcor" + x);
+				putString(10,15,terminal,"ycor" + y);//to clear leftover letters pad withspaces
+
 			}
 
-			terminal.moveCursor(x,y);
+
+
+		//	terminal.moveCursor(x,y);
 			// terminal.applyBackgroundColor(Terminal.Color.WHITE);
 			// terminal.applyForegroundColor(Terminal.Color.BLACK);
 			// //applySGR(a,b) for multiple modifiers (bold,blink) etc.
 			// terminal.applySGR(Terminal.SGR.ENTER_UNDERLINE);
-			//terminal.setCursorVisible(true);
+		// terminal.setCursorVisible(true);
 			// terminal.putCharacter('\u00a4');
 			// //terminal.putCharacter(' ');
 			// terminal.applyBackgroundColor(Terminal.Color.DEFAULT);
