3bf88a258a9a5ddec3ffd7d85880a60fa7eb1b4e
Christy Guan <cguan00@stuy.edu>
2019-01-21 10:24:57 -0500

    removed unnecessary old code

--- Move.java
+++ Move.java
@@ -25,12 +25,5 @@ public class Move {
     return capturedPiece;
   }
 
-  // not needed
-  // public Move(Board board, Player color, Square currentLoc, Square newLoc) {
-  //   currentPiece = currentLoc.getPiece(); //the piece to be moved is stored
-  //   currentLoc.storePiece(null); //the original square now doesn't have a piece
-  //   newLoc.storePiece(currentPiece); //the new square now gets the piece
-  //   currentPiece.setLocation(newLoc); //sets the location of the piece to the new square
-  // }
 
 }
